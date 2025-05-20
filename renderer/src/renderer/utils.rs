7c5eaecf0a82d50179d484265059191f7617f73f
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-05-20 10:04:44 +0900

    fix gltf transform

--- renderer/src/renderer/utils.rs
+++ renderer/src/renderer/utils.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 use anyhow::Result;
 use ash::vk;
+use gltf::{Node, buffer};
 
 use crate::renderer::{model_data::ModelData, vertex::Vertex, vulkan_state::VulkanState};
 
@@ -154,92 +155,132 @@ pub fn create_graphics_pipeline(
 pub fn load_glb(state: &mut VulkanState, path: impl AsRef<Path>) -> Result<Vec<ModelData>> {
     let mut model_data = vec![];
 
-    let (document, buffers, _images) = gltf::import(path)?;
-    let mesh = document.meshes().next().unwrap();
-    for primitive in mesh.primitives() {
-        let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+    fn traverse_gltf(
+        state: &mut VulkanState,
+        model_data: &mut Vec<ModelData>,
+        node: Node,
+        buffers: Vec<buffer::Data>,
+        parent_transform: glam::Mat4,
+    ) {
+        let local_transform = node.transform();
+        let transform =
+            parent_transform * glam::Mat4::from_cols_array_2d(&local_transform.matrix());
 
-        let indices = reader
-            .read_indices()
-            .unwrap()
-            .into_u32()
-            .collect::<Vec<_>>();
+        if let Some(mesh) = node.mesh() {
+            for primitive in mesh.primitives() {
+                let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
 
-        let mut vertices = vec![];
+                let indices = reader
+                    .read_indices()
+                    .unwrap()
+                    .into_u32()
+                    .collect::<Vec<_>>();
 
-        let positions = reader
-            .read_positions()
-            .unwrap()
-            .map(glam::Vec3::from)
-            .collect::<Vec<_>>();
-        let normals = reader
-            .read_normals()
-            .unwrap()
-            .map(glam::Vec3::from)
-            .collect::<Vec<_>>();
-        let uvs = if let Some(uvs) = reader.read_tex_coords(0) {
-            uvs.into_f32().map(glam::Vec2::from).collect::<Vec<_>>()
-        } else {
-            vec![glam::Vec2::ZERO; positions.len()]
-        };
+                let mut vertices = vec![];
 
-        let tangents = if let Some(tangents) = reader.read_tangents() {
-            tangents.map(glam::Vec4::from).collect::<Vec<_>>()
-        } else {
-            let mut tangents = vec![glam::Vec4::ZERO; positions.len()];
-            for is in indices.chunks(3) {
-                let i0 = is[0] as usize;
-                let i1 = is[1] as usize;
-                let i2 = is[2] as usize;
-
-                let p0 = positions[i0];
-                let p1 = positions[i1];
-                let p2 = positions[i2];
-
-                let uv0 = uvs[i0];
-                let uv1 = uvs[i1];
-                let uv2 = uvs[i2];
-
-                let edge1 = p1 - p0;
-                let edge2 = p2 - p0;
-
-                let delta_uv1 = uv1 - uv0;
-                let delta_uv2 = uv2 - uv0;
-
-                let r = 1.0 / (delta_uv1.x * delta_uv2.y - delta_uv1.y * delta_uv2.x);
-
-                let normal = edge1.cross(edge2).normalize();
-                let tangent = ((edge1 * delta_uv2.y - edge2 * delta_uv1.y) * r).normalize();
-                let bitangnet = ((edge2 * delta_uv1.x - edge1 * delta_uv2.x) * r).normalize();
-
-                let w = if normal.cross(tangent).dot(bitangnet) < 0.0 {
-                    -1.0
+                let positions = reader
+                    .read_positions()
+                    .unwrap()
+                    .map(glam::Vec3::from)
+                    .map(|p| transform.transform_point3(p))
+                    .collect::<Vec<_>>();
+                let normals = reader
+                    .read_normals()
+                    .unwrap()
+                    .map(glam::Vec3::from)
+                    .map(|n| {
+                        glam::Mat3::from_mat4(transform)
+                            .inverse()
+                            .transpose()
+                            .mul_vec3(n)
+                    })
+                    .collect::<Vec<_>>();
+                let uvs = if let Some(uvs) = reader.read_tex_coords(0) {
+                    uvs.into_f32().map(glam::Vec2::from).collect::<Vec<_>>()
                 } else {
-                    1.0
+                    vec![glam::Vec2::ZERO; positions.len()]
                 };
 
-                let tangent = glam::Vec4::new(tangent.x, tangent.y, tangent.z, w);
+                let is_mirrored = transform.determinant() < 0.0;
+                let tangents = if let Some(tangents) = reader.read_tangents() {
+                    tangents
+                        .map(glam::Vec4::from)
+                        .map(|t| {
+                            let tt = glam::Mat3::from_mat4(transform)
+                                .inverse()
+                                .transpose()
+                                .mul_vec3(t.truncate());
+                            let w = if is_mirrored { -t.w } else { t.w };
+                            glam::Vec4::new(tt.x, tt.y, tt.z, w)
+                        })
+                        .collect::<Vec<_>>()
+                } else {
+                    let mut tangents = vec![glam::Vec4::ZERO; positions.len()];
+                    for is in indices.chunks(3) {
+                        let i0 = is[0] as usize;
+                        let i1 = is[1] as usize;
+                        let i2 = is[2] as usize;
 
-                tangents[i0] = tangent;
-                tangents[i1] = tangent;
-                tangents[i2] = tangent;
+                        let p0 = positions[i0];
+                        let p1 = positions[i1];
+                        let p2 = positions[i2];
+
+                        let uv0 = uvs[i0];
+                        let uv1 = uvs[i1];
+                        let uv2 = uvs[i2];
+
+                        let edge1 = p1 - p0;
+                        let edge2 = p2 - p0;
+
+                        let delta_uv1 = uv1 - uv0;
+                        let delta_uv2 = uv2 - uv0;
+
+                        let r = 1.0 / (delta_uv1.x * delta_uv2.y - delta_uv1.y * delta_uv2.x);
+
+                        let normal = edge1.cross(edge2).normalize();
+                        let tangent = ((edge1 * delta_uv2.y - edge2 * delta_uv1.y) * r).normalize();
+                        let bitangnet =
+                            ((edge2 * delta_uv1.x - edge1 * delta_uv2.x) * r).normalize();
+
+                        let w = if normal.cross(tangent).dot(bitangnet) < 0.0 {
+                            -1.0
+                        } else {
+                            1.0
+                        };
+
+                        let tangent = glam::Vec4::new(tangent.x, tangent.y, tangent.z, w);
+
+                        tangents[i0] = tangent;
+                        tangents[i1] = tangent;
+                        tangents[i2] = tangent;
+                    }
+                    tangents
+                };
+
+                for i in 0..positions.len() {
+                    vertices.push(Vertex {
+                        pos: positions[i].into(),
+                        normal: normals[i].into(),
+                        tangent: tangents[i].into(),
+                        uv: uvs[i].into(),
+                    });
+                }
+                model_data.push(ModelData::new(state, &vertices, &indices).unwrap());
             }
-            tangents
-        };
-
-        for i in 0..positions.len() {
-            // println!(
-            //     "pos: {:?}, normal: {:?}, tangent: {:?}, uv: {:?}",
-            //     positions[i], normals[i], tangents[i], uvs[i]
-            // );
-            vertices.push(Vertex {
-                pos: positions[i].into(),
-                normal: normals[i].into(),
-                tangent: tangents[i].into(),
-                uv: uvs[i].into(),
-            });
         }
-        model_data.push(ModelData::new(state, &vertices, &indices)?);
+    }
+
+    let (document, buffers, _images) = gltf::import(path)?;
+    for scene in document.scenes() {
+        for node in scene.nodes() {
+            traverse_gltf(
+                state,
+                &mut model_data,
+                node,
+                buffers.clone(),
+                glam::Mat4::IDENTITY,
+            );
+        }
     }
 
     Ok(model_data)
